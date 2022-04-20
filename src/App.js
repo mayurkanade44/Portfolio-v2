c62f879f469be2609f4e326f3e26a2a29ef6f9a6
mayurkanade44 <mayurkanade44@gmail.com>
2022-04-20 12:22:17 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Home, Navbar, Projects, About, Contact } from "./components";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
