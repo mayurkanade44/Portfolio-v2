fe1af803fe2064cef2293f8862968aabf1d5f3cf
mayurkanade44 <mayurkanade44@gmail.com>
2022-04-28 14:54:52 +0530

    project card css changes

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,40 +3,34 @@ import internet from "../images/internet.png";
 
 const ProjectCard = ({ img, title, description, website, github }) => {
   return (
-    <div className="card-flip">
-      <div className="flip">
-        <div className="project-card-view">
-          <img
-            src={img}
-            className="card-img-top"
-            alt="project-img"
-            style={{ height: 200 }}
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-          </div>
-        </div>
-        <div className="back">
-          <div className="card">
-            <div className="card-body">
-              <div className="row m-3">
-                <div className="col-md-8">
-                  <h5 className="card-title">{title}</h5>
-                </div>
-                <div className="col-md-2">
-                  <a href={website} target="_blank" rel="noreferrer">
-                    <img src={internet} alt="internet" style={{ height: 35 }} />
-                  </a>
-                </div>
-                <div className="col-md-2">
-                  <a href={github} target="_blank" rel="noreferrer">
-                    <img src={git} alt="github" style={{ height: 30 }} />
-                  </a>
-                </div>
-              </div>
-              <p className="card-text text-info mb-5">{description}</p>
+    <div className="projects-card">
+      <img
+        src={img}
+        className="card-img-top"
+        alt="project-img"
+        style={{ height: 200 }}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+      </div>
+      <div className="projects-modal">
+        <div className="">
+          <div className="row my-2">
+            <div className="col-md-8">
+              <h5 className="card-title">{title}</h5>
+            </div>
+            <div className="col-md-2">
+              <a href={website} target="_blank" rel="noreferrer">
+                <img src={internet} alt="internet" style={{ height: 35 }} />
+              </a>
+            </div>
+            <div className="col-md-2">
+              <a href={github} target="_blank" rel="noreferrer">
+                <img src={git} alt="github" style={{ height: 30 }} />
+              </a>
             </div>
           </div>
+          <p className="card-text text-info my-3">{description}</p>
         </div>
       </div>
     </div>
