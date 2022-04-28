6aad01153d7dd14551de2bdbb1d9b523d9c0b89c
mayurkanade44 <mayurkanade44@gmail.com>
2022-04-28 18:08:47 +0530

    social icons css fix

--- src/components/Socialicons.jsx
+++ src/components/Socialicons.jsx
@@ -5,8 +5,8 @@ import linkedin from "../images/linkedin.png";
 
 const Socialicons = () => {
   return (
-    <>
-      <div className="col-md-1 m-1">
+    <div className="row">
+      <div className="col-md-1 social-icons">
         <a
           href="https://github.com/mayurkanade44"
           target="_blank"
@@ -15,12 +15,12 @@ const Socialicons = () => {
           <img src={github} alt="" id="github" />
         </a>
       </div>
-      <div className="col-md-1 m-1">
+      <div className="col-md-1 social-icons">
         <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
           <img src={linkedin} alt="" id="linkedin" />
         </a>
       </div>
-      <div className="col-md-1 m-1">
+      <div className="col-md-1 social-icons">
         <a
           href="https://twitter.com/redkopmayur"
           target="_blank"
@@ -30,7 +30,7 @@ const Socialicons = () => {
         </a>
       </div>
 
-      <div className="col-md-1 m-1">
+      <div className="col-md-1 social-icons">
         <a
           href="https://www.instagram.com/the_red_kop/"
           target="_blank"
@@ -39,7 +39,7 @@ const Socialicons = () => {
           <img src={insta} alt="" id="insta" />
         </a>
       </div>
-    </>
+    </div>
   );
 };
 export default Socialicons;
